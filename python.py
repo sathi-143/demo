bc2b099d9898f03f854bd669d270289a4a82fdf4
sathi-143 <satisharthala82@gmail.com>
2025-08-02 23:56:54 +0530

    Update python.py
    added the commands

--- python.py
+++ python.py
@@ -2,5 +2,7 @@ creating a file to learn fetch command
 first create a file in repository 
 go to git bash 
 use command git fetch 
-and gti pull 
-  
+after fetch give command git merge 
+and the file will be downloaded 
+if you use command git pull it will directly download to local repository
+
